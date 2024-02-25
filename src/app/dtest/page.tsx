78b32520f7593ba14d7921bba34918e7847134f5
SKYD0TS <SKYD0TS@users.noreply.github.com>
2024-02-25 23:45:23 +0700

    ONCLICK FUNCTION IMPLEMENTED!! HAHAHAH
    	modified:   src/app/dtest/page.tsx

--- src/app/dtest/page.tsx
+++ src/app/dtest/page.tsx
@@ -2,8 +2,15 @@
 
 import { createElement, useState } from "react"
 
-function insertFunction(elementFunctionName: string, elementFunctionArgs: Array<any>, handlers: Record<string, Function>, args: any) {
-    console.log("insertFunction", { elementFunctionName, elementFunctionArgs, handlers, args })
+function returnFunction(elementFunctionName: string, elementFunctionArgs: Array<any>, handlers: Record<string, Function>, args: any) {
+    console.log("returnFunction", { elementFunctionName, elementFunctionArgs, handlers, args })
+    const functionArguments = elementFunctionArgs.map((a: any) => a)
+    return () => { handlers[elementFunctionName](...functionArguments) }
+    // return functionInFunction
+}
+
+function returnFunctionValue(elementFunctionName: string, elementFunctionArgs: Array<any>, handlers: Record<string, Function>, args: any) {
+    console.log("returnFunctionValue", { elementFunctionName, elementFunctionArgs, handlers, args })
     const functionArguments = elementFunctionArgs.map((a: any) => a)
     const functionReturnValue = handlers[elementFunctionName](...functionArguments)
     return functionReturnValue
@@ -12,6 +19,28 @@ function insertFunction(elementFunctionName: string, elementFunctionArgs: Array<
 function createReactElement(elements: { type: string, props: any, children: any }, handlers?: Record<string, Function>, args?: any): any {
     console.log('===============')
     console.log('base elements', elements)
+
+    if (elements.type === 'button' && elements.props.onClick && handlers) {
+        elements.props.onClick = returnFunction(elements.props.onClick.function, elements.props.onClick.args, handlers, args)
+        console.log('this is button', elements.props)
+    }
+
+    //?? ITERATE THROUGH CHILDREN
+    if (Array.isArray(elements.children)) {
+        console.error('ITERATE THROUGH CHILDREN?', elements)
+        console.log('elements.children is ARRAY', elements.children)
+        return elements.children.map((child, childN) => {
+            console.log('child', child)
+            if (Array.isArray(child.children)) {
+                console.log(child)
+                const childElements = createReactElement(child.children, handlers, args)
+                const element = createElement(child.type, { ...child.props, key: childN }, childElements)
+                return element
+            }
+            console.log('createReactElement', child.type, { ...child.props, key: childN }, child.children)
+            return createReactElement({ type: child.type, props: { ...child.props, key: childN }, children: child.children }, handlers, args)
+        })
+    }
 
     //?? ITERATE THROUGH ELEMENTS
     if (Array.isArray(elements)) {
@@ -23,29 +52,12 @@ function createReactElement(elements: { type: string, props: any, children: any 
                 const elementObject = { type: element.type, props: { ...element.props, key: elementN }, children: element.children }
                 const elementChild = createReactElement(elementObject, handlers, args)
                 return createElement(element.type, { ...element.props, key: elementN }, elementChild)
-                // const ce = createElement(element.type, { ...element.props, key: elementN }, element.children)
-                // const parentElement = createReactElement()
-                // return parentElement
             } else if (element.function && handlers && args) {
-                const functionReturnValue = insertFunction(element.function, element.args, handlers, args)
+                const functionReturnValue = returnFunctionValue(element.function, element.args, handlers, args)
                 return functionReturnValue
             }
             console.log('element.child is NOT ARRAY/FUNCTION')
-            return createElement(element.type, { ...element.props, key: elementN }, element.children)
-        })
-    }
-
-    //?? ITERATE THROUGH CHILDREN
-    if (Array.isArray(elements.children)) {
-        console.log('elements.children is ARRAY', elements.children)
-        return elements.children.map((child, childN) => {
-            console.log('child', child)
-            if (Array.isArray(child.children)) {
-                const childElements = createReactElement(child.children, handlers, args)
-                const element = createElement(child.type, { ...child.props, key: childN }, childElements)
-                return element
-            }
-            return createReactElement({ ...child, props: { key: childN } }, handlers, args)
+            return createElement(element.type, { key: elementN, ...element.props }, element.children)
         })
     }
 
@@ -67,7 +79,7 @@ function createReactElement(elements: { type: string, props: any, children: any 
         const element = elements as any
         if (element.function && handlers && args) {
             console.log("ELEMENTS IS FUNCTION", element.function, element.args)
-            const functionReturnValue = insertFunction(element.function, element.args, handlers, args)
+            const functionReturnValue = returnFunctionValue(element.function, element.args, handlers, args)
             return functionReturnValue
         }
         console.log('elements has no child and is not a function, how?')
@@ -78,7 +90,8 @@ function createReactElement(elements: { type: string, props: any, children: any 
 export default function Page() {
 
     const functions: Record<string, Function> = {
-        addWord: (word1: string, word2: string) => word1 + ' ' + word2
+        addWord: (word1: string, word2: string) => word1 + ' ' + word2,
+        onClick: (word1: string, word2: string) => console.log({ word1 }, { word2 })
     }
 
     const [columns, setColumns] = useState([
@@ -88,75 +101,22 @@ export default function Page() {
             className: 'AWHF',
             enableSorting: false,
             children: [
-                { function: 'addWord', args: ['header', 'className'] },
                 {
                     type: 'button',
-                    props: { className: 'classname' },
-                    children: [{
-                        type: 'i',
-                        props: { className: 'classname' },
-                        children: [
-                            { function: 'addWord', args: ['header', 'className'] }
-                        ]
+                    props: {
+                        onClick: { function: 'onClick', args: ['header', 'className'] }
                     },
-                    { function: 'addWord', args: ['header', 'className'] }
-                    ],
-                    // { function: 'addWord', args: ['header', 'className'] }
+                    children: 'thisbutton',
                 },
-                // {
-                //     type: 'i',
-                //     props: { className: 'classname' },
-                //     children: 'just string',
-                // },
-                // {
-                //     type: 'br',
-                //     props: { className: 'classname' },
-                // },
-                // {
-                //     type: 'div',
-                //     props: { className: 'classname' },
-                //     children: [
-                //         {
-                //             type: 'p',
-                //             props: { className: 'classname' },
-                //             children: [{
-                //                 type: 'span',
-                //                 props: { className: 'classname' },
-                //                 children: 'd > p > span',
-                //             },
-                //             {
-                //                 type: 'br',
-                //                 props: { className: 'classname' },
-                //             },
-                //             {
-                //                 type: 'i',
-                //                 props: { className: 'classname' },
-                //                 children: 'just string',
-                //             },
-                //             ],
-                //         },
-                //     ],
-                // },
-                {
-                    type: 'i',
-                    props: { className: 'classname' },
-                    children: [{ function: 'addWord', args: ['header', 'className'] }],
-                },
-                // {
-                //     type: 'button',
-                //     props: { className: 'classname' },
-                //     children: [{
-                //         type: 'h3',
-                //         props: { className: 'classname' },
-                //         children: [{
-                //             type: 'i',
-                //             props: { className: 'classname' },
-                //             children: 'this be buttonih3',
-                //         }],
-                //     }],
-                // },
             ]
         },
+        // {
+        //     type: 'i',
+        //     props: { className: 'classname' },
+        //     children: [
+        //         { function: 'addWord', args: ['header', 'className'] }
+        //     ]
+        // },
         // {
         //     accessorKey: "username",
         //     header: "Username",
